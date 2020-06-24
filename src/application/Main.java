042afe49442b7626f8d52b71af8f02a2f8392cce
Rayllanderson <rayllanderson@gmail.com>
2020-06-24 01:10:40 -0300

    main tela

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,17 +5,23 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 
 public class Main extends Application {
     @Override
-    public void start(Stage stage) {
+    public void start(Stage primaryStage) {
 	try {
-	    Parent parent = FXMLLoader.load(getClass().getResource("/gui/View.fxml"));
-	    Scene scene = new Scene(parent);
-	    stage.setScene(scene);
-	    stage.show();
+	    FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+	    ScrollPane scrollPane = loader.load();
+	    
+	    scrollPane.setFitToHeight(true);
+	    scrollPane.setFitToWidth(true);
+	    
+	    Scene mainScene = new Scene(scrollPane);
+	    primaryStage.setScene(mainScene);
+	    primaryStage.setTitle("Sample JavaFX application");
+	    primaryStage.show();
 	} catch (IOException e) {
 	    e.printStackTrace();
 	}
